3df7706e45ed9f075a3af3f5c11e45da8a46eb35
dandi rahmawan <dandirahmawan95@gmail.com>
2020-10-08 20:01:55 +0700

    const

---
+++ src/const/const.js
@@ -0,0 +1,19 @@
+/*param = 1*/export const infoBiayaSpkluPerSite  = "Merupakan Biaya investasi untuk sebuah situs charging (SPKLU) tunggal."
+/*param = 2*/export const infoHargaJualPln       = "Berbasis tarif curah sebesar Rp 707 x Q, dengan 0,8 ≤ Q ≤ 2.";
+/*param = 3*/export const infoJumlahSpklu        = "Jumlah SPKLU yang pada tahun pertama akan dibangun diasumsikan sebanyak  rasio jumlah BEV yang ada tahun tsb/1 SPKLU."
+/*param = 4*/export const infoJumlahKendaraanListrik = "Merupakan asumsi jumlah kendaraan listrik yang berada dijalanan. Default value = 600 BEV Pada tahun pertama, dari data kemperin"
+/*param = 5*/export const infoHargaJualKonsumen  = "Menggunakan tarif layanan khusus sebesar Rp 1.650 x N, dengan N ≤ 1,5";
+/*param = 6*/export const infoKapasitas1Kbl      = "Kapasitas baterai BEV yang melakukan pengisian pada SPKLU"
+/*param = 7*/export const infoJumlahKonektor     = "Jumlah konektor yang disediakan pada sebuah EVSE"
+/*param = 8*/export const infoRugiDayaPendukung  = "Rugi-rugi sebuah EVSE dan daya pendukung (auxillary power) pada sebuah SPKLU (untuk lampu dll)";
+
+export const constInfoInput = [
+    infoBiayaSpkluPerSite,
+    infoHargaJualPln,
+    infoJumlahSpklu,
+    infoJumlahKendaraanListrik,
+    infoHargaJualKonsumen,
+    infoKapasitas1Kbl,
+    infoJumlahKonektor,
+    infoRugiDayaPendukung
+]
